07ef6d0f210fd31baa8c631433c61798d9ecb8e2
Austin Paxton <apaxton@teladoc.com>
2016-06-15 17:34:05 -0500

    added httpotion

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule AlexaTest.Mixfile do
   def application do
     [mod: {AlexaTest, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :httpotion]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,8 @@ defmodule AlexaTest.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     { :alexa, git: "https://github.com/gabiz/alexa"}]
+     {:alexa, git: "https://github.com/gabiz/alexa"},
+     {:httpotion, "~> 3.0.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
