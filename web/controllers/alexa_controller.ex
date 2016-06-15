24349b23294790228523d7ff2c1a937e59586685
Austin Paxton <apaxton@teladoc.com>
2016-06-15 16:02:50 -0500

    update end speech

--- web/controllers/alexa_controller.ex
+++ web/controllers/alexa_controller.ex
@@ -3,22 +3,23 @@ defmodule AlexaTest.AlexaController do
   use Alexa.Speech, :post
 
   def launchRequest(_params) do
-    response
+    response()
       |> set_output_speech("I am Alice, the wonderful bot. To deploy, try something like. Alexa, ask Alice to deploy call center to dev seven.") 
   end
 
   def sessionEndedRequest(_params) do
-    response
+    response()
+      |> set_output_speech("Goodbye.")
   end
 
   def intentRequest("HelloWorld", params) do    
-    response
+    response()
       |> set_output_speech("Watashi wa Alice dess!")
       |> set_should_end_session(true)
   end
 
   def intentRequest("AliceDeploy", params) do
-    response
+    response()
       |> set_output_speech("Deploying!")
       |> set_should_end_session(true)
   end
