26f3827329d9f6276a6d8e4821c561ffff7475c3
Austin Paxton <apaxton@teladoc.com>
2016-06-15 15:07:29 -0500

    log level changed

--- config/config.exs
+++ config/config.exs
@@ -18,7 +18,9 @@ use Mix.Config
 #
 # Or configure a 3rd-party app:
 #
-#     config :logger, level: :info
+config :logger,
+  level: :info,
+  compile_time_purge_level: :info
 #
 
 # It is also possible to import configuration files, relative to this
