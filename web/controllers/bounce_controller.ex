a752e7d838ccbcecaf516d8c13940df19d05ca8b
Austin Paxton <apaxton@teladoc.com>
2016-06-17 12:15:09 -0500

    fixed bad controller name

--- web/controllers/bounce_controller.ex
+++ web/controllers/bounce_controller.ex
@@ -2,7 +2,7 @@ defmodule AlexaTest.BounceController do
   use AlexaTest.Web, :controller
   
   def index(conn, _params) do
-    render json, TestServers.all_servers
+    json conn, TestServers.all_servers
   end
 
 end
